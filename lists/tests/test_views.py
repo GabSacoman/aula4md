f28e448797a3557bb7beb356ea44c416695e6764
GabSacoman <gabriel.sacoman@hotmail.com>
2023-06-07 19:34:05 -0300

    De volta ao teste funcional - PARTE 1

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -36,6 +36,16 @@ class ListViewTest(TestCase):
         response = self.client.get(f'/lists/{correct_list.id}/')
         self.assertEqual(response.context['list'], correct_list)
 
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -49,16 +59,6 @@ class ListViewTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            f'/lists/{correct_list.id}/',
-            data={'item_text': 'A new item for an existing list'}
-        )
-        self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
